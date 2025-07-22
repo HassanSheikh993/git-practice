0c0e26925363f727d54a7dda66d3879363e2da76
Hassan <hassansheikh993@gmail.com>
2025-07-22 14:57:09 +0500

    verifyAge function + name change

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const age = 21;
-const name = "Hassan";
+const name = "Hassan Sheikh";
 
 function display(){
     console.log(name + " " + age)
+}
+
+function verifyAge(){
+    if(age>=18){
+        console.log("PARTY")
+    }
+    console.log("In valid")
 }
